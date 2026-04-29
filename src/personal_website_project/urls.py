c5b75ab4ed56ad78dc68f1e4505d5d78b95ca7ab
SoerenNeumann <soerenneumann1@web.de>
2026-04-29 14:36:00 +0000

    Homepage view erzeugt

--- src/personal_website_project/urls.py
+++ src/personal_website_project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # << NEU
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("pages.urls")), # << NEU
 ]
