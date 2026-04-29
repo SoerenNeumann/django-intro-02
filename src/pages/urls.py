97393da0c5c49f7309d6c153c7c547f9e94e8c33
SoerenNeumann <soerenneumann1@web.de>
2026-04-29 14:45:10 +0000

    Über-mich view mit Template erzeugt

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_page_view
+from .views import home_page_view, about_page_view # << NEU
 
 urlpatterns = [
+    path("about/", about_page_view), # << NEU
     path("", home_page_view),
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render # << NEU
 
 def home_page_view(request):
     return HttpResponse("Homepage")
 
-# Create your views here.
+def about_page_view(request): # << NEU
+    return render(request, "pages/about.html")
