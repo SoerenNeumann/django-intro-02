0d0926007eb84aae4c00e58972371c10da7d2be1
SoerenNeumann <soerenneumann1@web.de>
2026-04-29 14:48:48 +0000

    Über-mich view mit Template erweitert

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render # << NEU
+from django.shortcuts import render
+from django import VERSION # < NEU
 
 def home_page_view(request):
     return HttpResponse("Homepage")
 
-def about_page_view(request): # << NEU
-    return render(request, "pages/about.html")
+def about_page_view(request): # < NEU
+    major, minor, micro, *_ = VERSION
+    context = {
+        "name": "Django", 
+        "profession": "Python-Webframework", 
+        "age": 20, 
+        "current_version": f"{major}.{minor}.{micro}"
+    }
+    return render(request, "pages/about.html", context)
